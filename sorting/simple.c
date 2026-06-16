4853d5ce86cecd3d5a61219b3be715c934ac7a59
Henrique <kikevdani@gmail.com>
2026-06-15 22:53:55 -0300

    adding sample operation

--- sorting/simple.c
+++ sorting/simple.c
@@ -1,9 +1,15 @@
-void	simple_sort(t_stack stack , unsigned char flags)
+void	simple_sort(t_stack stack , unsigned char flags, double disorder)
 {
 	t_stack	stack_b;
 	char	*operations;
 	int		operation_count[8];
 
 	create_stack(stack_b);
-	...
+	
+	... SORT
+
+	if (flags & FLAG_BENCHMARK)
+		create_benchmark("simple / 0(n2)", operation_count, operations, disorder);
+	else
+		ft_printf("%s\n", operations);
 }
